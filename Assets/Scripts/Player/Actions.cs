178b70346a05bf9b740d1061b038305f1c687f7b
Rearix <111284328+Rear9@users.noreply.github.com>
2025-09-23 16:21:20 +0100

    Mechanic Implementation

--- Assets/Scripts/Player/Actions.cs
+++ Assets/Scripts/Player/Actions.cs
@@ -14,18 +14,7 @@ public class Actions : MonoBehaviour
 
     private Rigidbody2D _rb;
 
-    void Awake()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-        _controls = new PlayerControls();
-        _controls.Player.Parry.performed += ctx => StartCoroutine(Parry());
-        _controls.Player.Dodge.performed += ctx => StartCoroutine(Dodge());
-    }
-
-    private void Dodge_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
-    {
-        throw new System.NotImplementedException();
-    }
+    private Coroutine _dodgeCoroutine;
 
     private void OnEnable() // Once player is active, enable controls
     {
@@ -37,15 +26,62 @@ public class Actions : MonoBehaviour
         _controls.Disable();
     }
 
-    private IEnumerator Dodge()
+    void Awake() // Initialize player inputs and subscribe actions to methods/respective coroutines
     {
-        _dodging = true;
-        yield break;
+        _rb = GetComponent<Rigidbody2D>();
+        _controls = new PlayerControls();
+        _controls.Player.Parry.performed += ctx => StartCoroutine(Parry());
+        _controls.Player.Dodge.performed += ctx => StartCoroutine(Dodge());
+
+        _controls.Player.Dodge.canceled += ctx => StartCoroutine(DodgeCancel());
     }
 
-    private IEnumerator Parry()
+    private bool CheckParrySuccess()
     {
-        _parrying = true;
+        // if parrying & hit collider => return true
+        return false;
+    }
+
+    private IEnumerator Dodge() // Player Dodge Input
+    {
+        if (_dodging) { yield return null; } 
+        _dodging = true; // Prevent multiple dodges activating at once
+        //logic
+        yield return new WaitForSeconds(dodgeWindow);
+        _dodging = false;
+        _dodgeCoroutine = null;
+        Debug.Log("Dodge");
+    }
+
+    private IEnumerator Parry() // Player Parry Input
+    {
+        if (_parrying) { yield return null; }
+        _parrying = true; // Prevent multiple parries activating at once
+        bool parrySuccess = false;
+        float timer = 0f; // Start timer
+        while (timer < parryWindow && !parrySuccess)
+        {
+            timer += Time.deltaTime;
+            if (CheckParrySuccess())
+            {
+                parrySuccess = true;
+            }
+            yield return null;
+        }
+        _parrying = false;
+        if (parrySuccess) {
+            Debug.Log("Parry");
+        }
+    }
+
+    private IEnumerator DodgeCancel()
+    {
+        if (_dodging && _dodgeCoroutine != null)
+        {
+            StopCoroutine(Dodge());
+            //cleanup
+            Debug.Log("DodgeCancel");
+        }
         yield break;
     }
 }
